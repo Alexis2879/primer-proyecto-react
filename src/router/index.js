aa8a544fd4f000f9e30c39b97639ef761dd34ce5
Alexis2879 <https://alexis2879.github.io/trabajo-salv/>
2022-03-22 23:36:06 -0500

    context

--- src/router/index.js
+++ src/router/index.js
@@ -9,11 +9,13 @@
  import { BrowserRouter, Routes, Route } from "react-router-dom";
  import Home from "../pages/Home";
  import Flags from "../pages/Flags";
+ import Detail from "../pages/Detail";
  import Youtube from "../pages/Youtube";
  import YoutubeAdministrator from "../pages/YoutubeAdministrador";
  import MovieUpdate from "../pages/MovieUpdate";
-
- import infobandera from "../pages/infobandera";
+ import Login from "../pages/Login";
+ // Layout
+ import Main from "../layouts/Main";
  
  // Nuestro Router va a ser un componente el cual se encargue de retornar
  // las rutas con su respectiva vista
@@ -22,20 +24,22 @@
    return (
      <BrowserRouter>
        <Routes>
-         <Route path="/" element={<Home />} />
-         <Route path="/flags" element={<Flags />} />
-         <Route path="/youtube" element={<Youtube />} />
-         <Route
-           path="/youtube/administrador"
-           element={<YoutubeAdministrator />}
-         />
-         <Route
-           path="/youtube/administrador/editar/:id"
-           element={<MovieUpdate />}
-         />
+         <Route element={<Main />}>
+           <Route path="/" element={<Home />} />
+           <Route path="/flags" element={<Flags />} />
+           <Route path="/flag/detail/:name" element={<Detail />} />
+           <Route path="/youtube" element={<Youtube />} />
+           <Route
+             path="/youtube/administrador"
+             element={<YoutubeAdministrator />}
+           />
+           <Route
+             path="/youtube/administrador/editar/:id"
+             element={<MovieUpdate />}
+           />
+         </Route>
+         <Route path="login" element={<Login />} />
        </Routes>
-       <Route path="/infobandera" element={<infobandera />} />
-      
      </BrowserRouter>
    );
  };
