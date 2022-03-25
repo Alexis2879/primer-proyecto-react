08e2350fe7aea00667102d546e378a2c6c47fe03
Alexis2879 <https://alexis2879.github.io/trabajo-salv/>
2022-03-24 23:10:40 -0500

    baket

--- src/service/firestore.js
+++ src/service/firestore.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBu0tNvcQBbGJXjfzm0-QzrHS6xnz3iFZ4",
@@ -10,8 +11,19 @@ const firebaseConfig = {
     measurementId: "G-QZ2CVQMKN0"
 };
 
-  const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+// Iniciar firestore
+// database : base de datos
+const db = getFirestore(app);
 
-  export default app;
-
+// Hacer la peticion para poder traer los productos
+export const getProductClothes = async () => {
+  // paso 1: Traer la coleccion de datos
+  const collectionClothes = collection(db, "product_clothes");
+  // paso 2: Traer los documentos
+  const documentClothes = await getDocs(collectionClothes);
+  // paso 3: Crear un arreglo que guarde los documentos que estamos obteniendo
+  const clothes = documentClothes.docs.map((doc) => doc.data());
+  return clothes;
+};
   
